f44eb753c8b45add43b6a7f6e3086bbea5853b55
thaidk <thaidamkhac03@gmail.com>
2024-12-17 07:25:19 +0700

    feat: add api recieve data from gg sheet
    feat: add api recieve data from gg sheet

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS  
 from controllers.auth_controller import login, logout
 from controllers.student_controller import get_student_detail
 from controllers.leaderboard_controller import get_leaderboard_scores, get_user_detail
+from models.student_model import get_bonus_points_by_student_id, get_bonus_points, delete_bonus_point, add_bonus_point
 import config
 import requests
 import webbrowser
@@ -22,6 +23,51 @@ def login_api():
 @app.route('/logout', methods=['POST'])
 def logout_api():
     return logout()
+
+# API để nhận dữ liệu từ Google Apps Script
+@app.route('/update', methods=['POST'])
+def update_data():
+    try:
+        # Lấy dữ liệu từ request
+        data = request.get_json()
+        
+        # In dữ liệu nhận được (để kiểm tra)
+        print("Received data:", data)
+        
+        # Xử lý dữ liệu
+        sheet_name = data.get("sheetName")
+        row = data.get("row")
+        col = data.get("col")
+
+        rowData = data.get("rowData")
+        
+        bonusPoint = rowData[23]
+        
+        studentId = rowData[1]
+        
+        aWardedDate = data.get("columnEdited")
+        
+        sumBonusPoint = get_bonus_points_by_student_id(studentId).get("sum")
+        
+        if (bonusPoint, sumBonusPoint, studentId, aWardedDate is not None):
+
+            if(bonusPoint < sumBonusPoint):
+                bonusPointId = get_bonus_points(studentId, aWardedDate).get("id")
+                if(bonusPointId is None):
+                    raise ValueError("bonusPoint cannot be found")
+                delete_bonus_point(bonusPointId)
+            elif(bonusPoint > sumBonusPoint):
+                add_bonus_point(studentId, aWardedDate)
+
+        return jsonify({"status": "success", "message": "Data received and processed"}), 200
+    except Exception as e:
+        # Xử lý lỗi
+        print("Error:", str(e))
+        return jsonify({"status": "error", "message": str(e)}), 500
+    except ValueError as ve:
+        # Xử lý lỗi
+        print("Error:", str(ve))
+        return jsonify({"status": "error", "message": str(ve)}), 500
 
 app.add_url_rule('/students', 'get_student_detail', get_student_detail, methods=['GET'])
 
